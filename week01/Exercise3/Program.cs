b80637782e885436520113cfe4005f3f9da10c54
Sauriodont <sauriodont@gmail.com>
2025-07-04 02:18:01 -0300

    exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response;
+        Random randomGenerator = new Random();
+        Console.WriteLine("Try to guess the magic number");
+        do
+        {
+            int number = randomGenerator.Next(1, 100);
+            int guess = -1;
+            while (guess != number){
+            Console.Write("What is your guess?");
+            String gues = Console.ReadLine();
+            guess = int.Parse(gues);
+            
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You made it");
+                }
+
+
+            }
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
