dcfe12c90649987d2f80b6371a94ed6d48a1f672
sauriodont <sauriodont1@gmail.com>
2025-08-04 22:25:41 -0300

    week end

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment n1 = new Assignment("Linda Waters", "Math Expresions");
+        Console.WriteLine(n1.GetSummary());
+
+        MathAssignment n2 = new MathAssignment("Albert Connor", "Square root", "18", "15-22");
+        Console.WriteLine(n2.GetSummary());
+        Console.WriteLine(n2.GetHomeworkList());
+
+        WritingAssignment n3 = new WritingAssignment("Lucy Hamilton", "American History", "USA Independency");
+        Console.WriteLine(n3.GetSummary());
+        Console.WriteLine(n3.GetWritingInformation());
     }
 }
