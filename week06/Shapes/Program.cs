36ceaffc57daa5f5505d904d3198754e82e76aae
sauriodont <sauriodont1@gmail.com>
2025-08-10 21:09:00 -0300

    shapes ended

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shapes> shape = new List<Shapes>();
+
+        Square s1 = new Square("Yellow", 5);
+        shape.Add(s1);
+
+        Rectangle s2 = new Rectangle("White", 6, 7);
+        shape.Add(s2);
+
+        Circle s3 = new Circle("Red", 9);
+        shape.Add(s3);
+
+        foreach (Shapes s in shape)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
---
+++ week06/Shapes/Shapes.cs
@@ -0,0 +1,18 @@
+public abstract class Shapes
+{
+    private string _color;
+
+    public Shapes(string color)
+    {
+        _color = color;
+    }
+    public string GetColor()
+    {
+        return _color;
+    }
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    public abstract double GetArea();
+}
